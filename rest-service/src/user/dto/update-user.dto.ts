64bded29216581c24f3d10b1a2c67220f51f0125
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-07-26 11:01:16 +0300

    fix: remove unnecessary plugs

--- rest-service/src/user/dto/update-user.dto.ts
+++ rest-service/src/user/dto/update-user.dto.ts
@@ -7,14 +7,11 @@ import {
     Min,
     Max,
 } from 'class-validator';
-const plugLogin = 'a';
-const plugPassword = 'Qw2';
-const plugAge = 5;
 export class UpdateUserDto {
     @IsDefined()
     @IsNotEmpty()
     @IsString()
-    readonly login: string = plugLogin;
+    readonly login: string;
 
     @IsDefined()
     @IsNotEmpty()
@@ -22,11 +19,11 @@ export class UpdateUserDto {
     @Matches(/(?=.*[0-9])(?=.*[a-z]|[A-Z])/i, {
         message: 'password must contain letters and numbers',
     })
-    readonly password: string = plugPassword;
+    readonly password: string;
 
     @IsNotEmpty()
     @IsNumber()
     @Max(130)
     @Min(4)
-    readonly age: number = plugAge;
+    readonly age: number;
 }
